f0eb565fe2acf0e2c3d2456f683737bbafa7a9e5
KentaKamikokuryo <84831144+KentaKamikokuryo@users.noreply.github.com>
2023-05-28 12:58:07 +0900

    update

--- djangosnippets/snippets/models.py
+++ djangosnippets/snippets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Snippet(models.Model):
+
+    title = models.CharField("Title", max_length=128)                  # スニペットのタイトルを格納するフィールド．models.CharFieldで定義し，最大128文字とした．
+    code = models.TextField("Code", blank=True)                        # スニペットを誰が投稿したのかわかるように，このフィールドではユーザーモデルの主キーへの参照を持っている．
+    description = models.TextField("Description", blank=True)          # ソースコードを格納するフィールド
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                   verbose_name="Contributor",
+                                   on_delete=models.CASCADE)
+    created_at = models.DateTimeField("Posted day", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated day", auto_now=True)
+
+    def __str__(self):
+        return self.title
+
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def top(request):
     return HttpResponse(b"Hello World")
+
+
+def snippet_new(request):
+    return HttpResponse("Registering snippets")
+
+
+def snippet_edit(request, snippet_id):
+    return HttpResponse("Editing snippets")
+
+
+def snippet_detail(request, snippet_id):
+    return HttpResponse("Browsing snippet details")
